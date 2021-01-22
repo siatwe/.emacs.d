5c29745b02beb89eed894d213226f14391db95bb
Simon Weis <simonattilaweis@tutanota.com>
2021-01-22 21:29:40 +0100

    add .gitignore; update Emacs functions; rename Emacs main config;

--- init.el
+++ init.el
@@ -15,17 +15,4 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(org-babel-load-file "~/.emacs.d/configuration.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(fzf org-evil visual-fill-column bongo pdf-tools tablist disk-usage rainbow-delimiters browse-kill-ring scratch windower expand-region eshell-did-you-mean eshell-up web-mode beacon which-key org-bullets hl-todo php-cs-fixer php-mode dracula-theme doom-modeline counsel-projectile projectile all-the-icons yasnippet lsp-treemacs treemacs-evil treemacs lsp-ui lsp-mode company helpful ivy-prescient flx counsel ivy-rich ivy emmet-mode magit diff-hl key-chord undo-tree evil-commentary evil-multiedit evil-matchit evil-leader evil-org evil-collection evil auto-compile use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(org-babel-load-file "~/.emacs.d/Emacs.org")
