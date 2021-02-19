dcfed4e64c87120083108c40bac9341c9049e2bf
Simon Weis <simonattilaweis@tutanota.com>
2021-02-19 22:30:53 +0100

    Emacs: Add lsp-ui

--- init.el
+++ init.el
@@ -21,8 +21,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(git-gutter:update-interval 2)
+ '(org-agenda-files '("~/.org/people.org" "~/.org/todo.org"))
  '(package-selected-packages
-   '(git-gutter all-the-icons-ivy dired-k dired-git-info org-tree-slide yasnippet windower which-key web-mode visual-fill-column use-package undo-tree typescript-mode treemacs-projectile treemacs-evil scratch rainbow-delimiters php-mode php-cs-fixer pdf-tools org-evil org-bullets magit lsp-ui lsp-treemacs key-chord ivy-rich ivy-prescient hl-todo helpful general fzf flx expand-region evil-org evil-multiedit evil-matchit evil-leader evil-commentary evil-collection eshell-up eshell-did-you-mean emmet-mode dracula-theme doom-modeline disk-usage diff-hl counsel-projectile company browse-kill-ring bongo beacon auto-compile)))
+   '(tree-sitter-langs git-gutter all-the-icons-ivy dired-k dired-git-info org-tree-slide yasnippet windower which-key web-mode visual-fill-column use-package undo-tree typescript-mode treemacs-projectile treemacs-evil scratch rainbow-delimiters php-mode php-cs-fixer pdf-tools org-evil org-bullets magit lsp-ui lsp-treemacs key-chord ivy-rich ivy-prescient hl-todo helpful general fzf flx expand-region evil-org evil-multiedit evil-matchit evil-leader evil-commentary evil-collection eshell-up eshell-did-you-mean emmet-mode dracula-theme doom-modeline disk-usage diff-hl counsel-projectile company browse-kill-ring bongo beacon auto-compile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
